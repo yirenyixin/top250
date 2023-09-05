2c71f63c2ae9ae5ab3f724daf729dc173566ab8e
yirenyixin <1595668021l@qq.com>
2023-09-05 18:06:36 +0800

    0905

--- python/数据清洗.py
+++ python/数据清洗.py
@@ -16,17 +16,33 @@ sheet = wb.active
 data = [
     ["ID","名称","标签"],
 ]
-i=0
+i=1
 # 指定要创建的 CSV 文件的路径
 csv_file_path = "example.csv"
 
 # 使用 "w" 模式打开 CSV 文件，创建文件并写入数据
 with open(csv_file_path, "w", newline="",encoding="utf-8") as csv_file:
     csv_writer = csv.writer(csv_file)
-    for row in sheet.iter_rows(min_col=1, max_col=1):
+    for row in sheet.iter_rows(min_row=2,min_col=1, max_col=1):
         for cell in row:
             data.append([i,cell.value,"电影名称"])
             i+=1
+    for row in sheet.iter_rows(min_row=2,min_col=5, max_col=5):
+        for cell in row:
+            # 使用 \n 拆分字符串并获取拆分后的第一个元素，然后去除多余的空格和换行符
+            first_value = cell.value.split('\n')[0].strip()
+            # 打印第一个元素
+            print(first_value)
+            # data.append([i,cell.value,"导演名"])
+            # i+=1
+    # for row in sheet.iter_rows(min_col=5, max_col=5):
+    #     for cell in row:
+    #         data.append([i,cell.value,"演员名"])
+    #         i+=1
+    # for row in sheet.iter_rows(min_col=5, max_col=5):
+    #     for cell in row:
+    #         data.append([i,cell.value,"类型"])
+    #         i+=1
 with open(csv_file_path, "w", newline="", encoding="utf-8") as csv_file:
     csv_writer = csv.writer(csv_file)
     for row in data:
